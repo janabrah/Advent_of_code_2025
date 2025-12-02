5c5002dc2b3060c6f53ab1bf57dbf3e4468b883d
Coby Abrahams <coby@meter.com>
2025-12-01 21:19:03 -0800

    Sloppy version of day 2

--- cmd/day_2/main.go
+++ cmd/day_2/main.go
@@ -2,29 +2,113 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/janabrah/Advent_of_code_2025/utils"
 )
 
 func main() {
-	input, err := getFiles("example")
+	input, err := getFiles("real")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	partOne(input)
+	partTwo(input)
 }
 
 func partOne(input []string) {
-	fmt.Println(input[0])
+	entries := strings.Split(input[0], ",")
+	total := 0
+	for _, v := range entries {
+		ends := strings.Split(v, "-")
+		start, err := strconv.Atoi(ends[0])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		end, err := strconv.Atoi(ends[1])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		total += findInvalids(start, end)
+	}
+	fmt.Println(total)
+}
+
+func findInvalids(start int, end int) int {
+	fmt.Println(start, end)
+	total := 0
+	for i := start; i <= end; i++ {
+		if isInvalid(strconv.Itoa(i)) {
+			total += i
+		}
+	}
+	return total
+}
+
+func isInvalid(val string) bool {
+	length := len(val)
+	if length%2 != 0 {
+		return false
+	}
+	if val[:length/2] == val[length/2:] {
+		return true
+	}
+	return false
+}
+
+func findInvalids2(start int, end int) int {
+	fmt.Println(start, end)
+	total := 0
+	for i := start; i <= end; i++ {
+		if isInvalid2(strconv.Itoa(i)) {
+			total += i
+		}
+	}
+	return total
+}
+
+func isInvalid2(val string) bool {
+	length := len(val)
+	checked := false
+loop:
+	for i := 1; i <= length/2; i++ {
+		if length%i == 0 {
+			for j := 1; j < (length / i); j++ {
+				if val[:i] != val[i*j:i*(j+1)] {
+					continue loop
+				}
+			}
+			checked = true
+		}
+	}
+	return checked
 }
 
 func partTwo(input []string) {
-	fmt.Println(input[0])
+	entries := strings.Split(input[0], ",")
+	total := 0
+	for _, v := range entries {
+		ends := strings.Split(v, "-")
+		start, err := strconv.Atoi(ends[0])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		end, err := strconv.Atoi(ends[1])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		total += findInvalids2(start, end)
+	}
+	fmt.Println(total)
 }
 
 func getFiles(version string) ([]string, error) {
-	file, err := utils.LoadFile("day_3", version)
+	file, err := utils.LoadFile("day_2", version)
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
